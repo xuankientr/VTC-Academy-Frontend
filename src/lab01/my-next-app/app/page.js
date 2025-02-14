02b6d04d44fc4fcd204a8fffa1a6b9e095b417c5
xuankientr <xuankientr@gmail.com>
2025-02-14 15:33:36 +0700

    Initial commit

---
+++ src/lab01/my-next-app/app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>NextJS with JSX</h1>
+      <p>Welcome to the world of NextJS!</p>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ul>
+    </div>
+  );
+}
